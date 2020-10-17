4f259ccb0a227159846cedb6bf20fc76af2f09c9
hulya balci <hulyabalci07@gmail.com>
2020-10-17 13:05:06 +0200

    added DashboardPage

---
+++ src/test/java/com/cybertek/pages/DashboardPage.java
@@ -0,0 +1,16 @@
+package com.cybertek.pages;
+
+import com.cybertek.pages.BasePage;
+
+public class DashboardPage extends BasePage {
+
+    //no need to explicitly write constructor, bc it will use parent class cons
+
+
+
+
+
+
+
+
+}
